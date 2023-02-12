e823949eccef56d8768b9bc18c6187c5f4df7512
hasanbilgintr <hasanbilgintr@gmail.com>
2023-02-12 05:57:18 +0300

    JavaFragment

---
+++ app/src/main/java/com/hasanbilgin/javafragment/MainActivity.java
@@ -0,0 +1,22 @@
+package com.hasanbilgin.javafragment;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void  goToFirst(View view){
+
+    }
+    public void  goToSecond(View view){
+
+    }
+}
